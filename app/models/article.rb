f387878d53a4a7503bee2925919e4641c4d4f431
Casey Gollan <casey@bullshit.systems>
2018-05-18 16:52:52 -0400

    better models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_one :user
+  belongs_to :user, :foreign_key => "user_id", :class_name => "User"
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, :foreign_key => "user_id", :class_name => "Article"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
